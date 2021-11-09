3b4b41ac05e033b8212e96aabc105709acd3f941
leonelcarrossoni <leonel.carrossoni@gmail.com>
2021-11-09 10:05:32 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { TaskList } from './components/TaskList'
 import { Header } from "./components/Header";
 import './styles/global.scss'
 
-
+//teste
 export function App() {
   return (
     <>
